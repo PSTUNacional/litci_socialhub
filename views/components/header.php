b605d309db84bc7d7ff1d47220721ec8c777930f
romerito.pontes <romerito.pontes@monitoratec.com.br>
2025-01-18 11:39:18 -0300

    Cache issue

--- views/components/header.php
+++ views/components/header.php
@@ -7,6 +7,10 @@
 
     <meta property="og:title" content="Social Hub | IWL-FI" />
 
-    <link rel="stylesheet" href="/assets/css/style.css?v=<?=time();?>">
+    <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate">
+    <meta http-equiv="Pragma" content="no-cache">
+    <meta http-equiv="Expires" content="0">
+    <link rel="stylesheet" href="/assets/css/style.css?v=<?= time(); ?>">
+    
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/lipis/flag-icons@7.2.3/css/flag-icons.min.css" />
