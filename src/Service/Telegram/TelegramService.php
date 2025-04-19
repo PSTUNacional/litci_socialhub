47ae9066a8473c47e032b46381e5c038b227b32d
romerito.pontes <romerito.pontes@monitoratec.com.br>
2025-04-19 07:47:56 -0300

    Telegram Handlers test

---
+++ src/Service/Telegram/TelegramService.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace SH\Service\Telegram ;
+
+use SH\Service\Telegram\Handlers\Message;
+
+class TelegramService
+{
+
+    public function sendMessage(int|string $chatId, string $text, ?string $url = null, string $label = 'Clique aqui'): array
+    {
+        $message = new Message();
+        return $message->sendMessage($chatId, $text, $url, $label);
+    }
+
+    public function sendImage(int|string $chatId, string $imageUrl, string $caption = ''): array
+    {
+        $message = new Message();
+        return $message->sendImage($chatId, $imageUrl, $caption);
+    }
+    
+    public function dispatch(array $update): void
+    {
+        $message = new Message($update);
+        $text = $message->getText();
+
+        switch(true){
+            case str_starts_with($text, '/start'):
+                (new Handlers\StartHandler())->handle($message);
+                break;
+
+            case str_starts_with($text, '/help'):
+                $message->reply('Comandos disponíveis: /start, /help');
+                break;
+
+            default:
+                $message->reply('Comando não reconhecido. Use /help para ver os comandos disponíveis.');
+                break;
+        }
+    }
+}
